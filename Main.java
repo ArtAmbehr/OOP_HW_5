b07b31029d7e4108b271a92195748f5428be8d10
Art <art-75@yandex.ru>
2023-03-17 19:20:31 +0300

    Main

---
+++ Main.java
@@ -0,0 +1,19 @@
+package Seminar11.calc;
+
+import Seminar11.calc.controller.UserController;
+import Seminar11.calc.model.Logger;
+import Seminar11.calc.model.LoggerImpl;
+import Seminar11.calc.model.CalcComplex;
+import Seminar11.calc.model.CalcRational;
+import Seminar11.calc.view.ViewUser;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = new LoggerImpl("calc.txt");
+        CalcRational calcRational = new CalcRational(logger);
+        CalcComplex calcComplex = new CalcComplex(logger);
+        UserController controller = new UserController(calcRational, calcComplex);
+        ViewUser view = new ViewUser(controller);
+        view.run();
+    }
+}
