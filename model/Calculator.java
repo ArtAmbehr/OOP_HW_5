2de40868aed575cd8fdbe607b0b6b5b43d8b6a94
Art <art-75@yandex.ru>
2023-03-17 19:29:44 +0300

    Calculator

---
+++ model/Calculator.java
@@ -0,0 +1,26 @@
+package Seminar11.calc.model;
+
+public abstract class Calculator implements Calcable {
+    public final String addition = "+";
+    public final String subtract = "-";
+    public final String multiply = "*";
+    public final String division = "/";
+    private final Logger logger;
+
+    public Calculator(Logger logger) {
+        this.logger = logger;
+    }
+
+
+    @Override
+    public void help() {
+        System.out.println("Help: i could work with rational" +
+                " and complex numbers! That's all");
+    }
+
+    public void saveResult(String result) {
+        logger.saveLine(result);
+    }
+
+
+}
